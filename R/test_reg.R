9d8bc61e2aa68becb2f7e9ddde279cc7bcfde0ad
XiaoouPan <xip024@ucsd.edu>
2022-01-19 20:13:02 -0800

    growing scale test

--- R/test_reg.R
+++ R/test_reg.R
@@ -16,67 +16,8 @@ exam = function(beta, betaHat) {
   return (sqrt(mean((betaHat - beta)^2)))
 }
 
-## ab example with fixed scale
-n = 400
-p = 50
-Sigma = toeplitz(0.5^(0:(p - 1)))
-alpha = 0.05
-M = 50
-coef1 = coef2 = time = matrix(0, M, 4)
-
-pb = txtProgressBar(style = 3)
-for (i in 1:M) {
-  set.seed(i)
-  X = mvrnorm(n, rep(0, p), Sigma)
-  err = rt(n, 2)
-  ## Homo 
-  beta = runif(p, 0, 2)
-  Y = X %*% beta + err
-  beta_qr = c(qt(alpha, 2), beta)
-  integrand = function(x) {qt(x, 2)}
-  inte = integrate(integrand, lower = 0, upper = alpha)
-  beta_es = c(inte$value / alpha, beta)
-  ## Hetero
-  #X[, 1] = abs(X[, 1])
-  #beta = runif(p - 1, 0, 2)
-  #Y = X[, 1] * err + X[, -1] %*% beta + rnorm(n)
-  
-  start = Sys.time()
-  fit1 = esreg(Y ~ X, alpha = alpha)
-  end = Sys.time()
-  time[i, 1] = as.numeric(difftime(end, start, units = "secs"))
-  coef1[i, 1] = exam(beta_qr, fit1$coefficients_q)
-  coef2[i, 1] = exam(beta_es, fit1$coefficients_e)
-  
-  start = Sys.time()
-  fit2 = twoStepNonstd(X, Y, alpha = alpha)
-  end = Sys.time()
-  time[i, 2] = as.numeric(difftime(end, start, units = "secs"))
-  coef1[i, 2] = exam(beta_qr, fit2$beta)
-  coef2[i, 2] = exam(beta_es, fit2$theta)
-  
-  start = Sys.time()
-  fit3 = twoStep(X, Y, alpha = alpha)
-  end = Sys.time()
-  time[i, 3] = as.numeric(difftime(end, start, units = "secs"))
-  coef1[i, 3] = exam(beta_qr, fit3$beta)
-  coef2[i, 3] = exam(beta_es, fit3$theta)
-  
-  start = Sys.time()
-  fit4 = twoStepRob(X, Y, alpha = alpha)
-  end = Sys.time()
-  time[i, 4] = as.numeric(difftime(end, start, units = "secs"))
-  coef1[i, 4] = exam(beta_qr, fit4$beta)
-  coef2[i, 4] = exam(beta_es, fit4$theta)
-  
-  setTxtProgressBar(pb, i / M)
-}
-
-rbind(colMeans(time), colMeans(coef1), colMeans(coef2))
-
-
-nseq = seq(500, 2000, by = 500)
-pseq = floor(nseq / 50)
+nseq = seq(1000, 10000, by = 1000)
+pseq = floor(nseq / 100)
 l = length(nseq)
 alpha = 0.05
 qr0 = qt(alpha, 2)
@@ -113,7 +54,7 @@ for (j in 1:l) {
     end = Sys.time()
     time1[i, j] = as.numeric(difftime(end, start, units = "secs"))
     qr1[i, j] = exam(beta_qr, fit1$coefficients_q)
-    es2[i, j] = exam(beta_es, fit1$coefficients_e)
+    es1[i, j] = exam(beta_es, fit1$coefficients_e)
     
     start = Sys.time()
     fit2 = twoStep(X, Y, alpha = alpha)
@@ -132,3 +73,7 @@ for (j in 1:l) {
     setTxtProgressBar(pb, ((j - 1) * M + i) / (l * M))
   }
 }
+
+rbind(colMeans(time1), colMeans(time2), colMeans(time3))
+rbind(colMeans(qr1), colMeans(qr2), colMeans(qr3))
+rbind(colMeans(es1), colMeans(es2), colMeans(es3))
